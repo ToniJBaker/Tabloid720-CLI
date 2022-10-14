2bbff2fdbf4431685e328fb9aa9c96d62fb965b1
Toni Baker <tonijbaker5@gmail.com>
2022-10-14 10:23:49 -0400

    journalcs

--- TabloidCLI/Models/Journal.cs
+++ TabloidCLI/Models/Journal.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TabloidCLI.Models
 {
-    internal class Journal
+    public class Journal
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        
     }
 }
