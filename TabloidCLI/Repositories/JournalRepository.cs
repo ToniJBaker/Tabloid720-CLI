4e15dfa8894846a073e391409e4ffc91e38f71ae
Toni Baker <tonijbaker5@gmail.com>
2022-10-14 11:27:39 -0400

    journal manager

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
-namespace TabloidCLI.Repositories
+namespace TabloidCLI
 {
-    internal class JournalRepository
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
     {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+       
+        //Get all information for listing journal entries
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                               Title
+                                               Content
+                                                CreateDateTime
+                                          FROM Journal";
+
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                          
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
 }
