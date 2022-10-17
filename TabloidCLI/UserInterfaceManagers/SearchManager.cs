87db8d10e24d50ac5786c2434f27bd9ca79d5aca
Toni Baker <tonijbaker5@gmail.com>
2022-10-17 13:54:15 -0400

    search

--- TabloidCLI/UserInterfaceManagers/SearchManager.cs
+++ TabloidCLI/UserInterfaceManagers/SearchManager.cs
@@ -28,6 +28,7 @@ namespace TabloidCLI.UserInterfaceManagers
             switch (choice)
             {
                 case "1":
+                    SearchBlogs();
                     return this;
                 case "2":
                     SearchAuthors();
@@ -43,6 +44,25 @@ namespace TabloidCLI.UserInterfaceManagers
                     return this;
             }
         }
+
+
+        //private void SearchBlogs()
+        //{
+        //    Console.Write("Tag> ");
+        //    string tagName = Console.ReadLine();
+
+        //    SearchResults<Blog> results = _tagRepository.SearchBlogs(tagName);
+
+        //    if (results.NoResultsFound)
+        //    {
+        //        Console.WriteLine($"No results for {tagName}");
+        //    }
+        //    else
+        //    {
+        //        results.Display();
+        //    }
+        //}
+
 
         private void SearchAuthors()
         {
